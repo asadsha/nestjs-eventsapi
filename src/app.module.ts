e94a97b2d5d55517b9d6020bf9d29df8c4071ab9
Asad Shah <asadsha6131@gmail.com>
2020-11-05 16:10:15 +0500

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { EventsModule } from './event/event.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig), EventsModule, AuthModule],
+  controllers: [AppController],
 })
 export class AppModule { }
