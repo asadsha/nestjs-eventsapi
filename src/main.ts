e48e3263d1119ca90290174ab2b1522dc8967584
Asad Shah <asadsha6131@gmail.com>
2020-11-06 12:17:03 +0500

    initial commits

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import * as env from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+env.config();
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-
-  const port = process.env.PORT || 3000;
+  const port = parseInt(process.env.PORT) || 3000;
   await app.listen(port);
   logger.log(`Application started on port ${port}`);
 
